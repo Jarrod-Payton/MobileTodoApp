717b81813a6575ada5a7a7e7ac46d15f8b2b16ce
Jarrod-Payton <jarrodlpayton@gmail.com>
2023-09-14 23:29:56 -0500

    Basic Adjustments

--- components/InputTodo.js
+++ components/InputTodo.js
@@ -1,12 +1,37 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function InputTodo() {
   return (
-    <View style>
-      <Text>Todo Input</Text>
-      <TextInput defaultValue="Hello World" />
+    <View style={styles.parent}>
+      <Pressable style={styles.clearBtn}>
+        <Text>Clear</Text>
+      </Pressable>
+
+      <TextInput style={styles.inputField} defaultValue="Hello World" />
+
+      <Pressable style={styles.addBtn}>
+        <Text>Add</Text>
+      </Pressable>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  parent: {
+    width: "100%",
+    backgroundColor: "gray",
+    display: "flex",
+    flexDirection: "row",
+    alignitems: "center",
+    justifyContent: "space-between",
+  },
+  inputField: {},
+  clearBtn: {
+    backgroundColor: "red",
+    padding: 10,
+  },
+  addBtn: {
+    backgroundColor: "blue",
+    padding: 10,
+  },
+});
